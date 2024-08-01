8169e29d445280fcf857917f0b107a846ea20d7c
Andrei Roh <andrei.roh@icloud.com>
2024-08-01 18:34:14 +0200

    ODSB-4: Update getHTMLCaption function according to restrictions of telegram API

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const APOD_URL = 'https://api.nasa.gov/planetary/apod';
+export const MAX_DESCRIPTION_LENGTH = 1000;
--- src/utils/getHTMLCaption.ts
+++ src/utils/getHTMLCaption.ts
@@ -1,8 +1,13 @@
+import { MAX_DESCRIPTION_LENGTH } from "../constants";
+
 export const getHTMLCaption = (
   title: string,
   description: string,
   date: string
 ) =>
   `<b>${title}</b>
-${description}
+${description.length >= MAX_DESCRIPTION_LENGTH 
+  ? `${description.slice(0, MAX_DESCRIPTION_LENGTH * 0.85)}... 
+More: https://andrei-roh.github.io/andrei-roh/` 
+  : description}
 <i>${date}</i>`;
